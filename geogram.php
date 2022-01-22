e87b4cfbe717f42108af55867dfd507dd9a569c2
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2022-01-22 04:32:47 -0500

    added getting data from the API

--- geogram.php
+++ geogram.php
@@ -1,5 +1,19 @@
 <?php
+    if( !empty($_GET['location']) ) {
+        $maps_url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($_GET['location']);
+        
+        $maps_json = file_get_contents($maps_url);
 
+        $maps_array = json_decode($maps_json, true);
+
+        $lat = $maps_array['results'][0]['geometry']['location']['lat'];
+        $lng = $maps_array['results'][0]['geometry']['location']['lng'];
+
+        $instagram_url = 'https://api.instagram.com/v1/media/search?lat=' . $lat . '&lng=' . $lng . '&client_id=59cd273f121d4139b97a8a027a993ddf';
+
+        $instagram_json = file_get_contents($instagram_url);
+        $instagram_array = json_decode($instagram_json, true);
+    }
 ?>
 
 <!DOCTYPE html>
